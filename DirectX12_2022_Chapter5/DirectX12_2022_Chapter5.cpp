506551fdfc0c96d465c61ac9d91029f190665e2f
Rukia <syaru223@gmail.com>
2022-09-02 10:10:10 +0900

    new

--- DirectX12_2022_Chapter5/DirectX12_2022_Chapter5.cpp
+++ DirectX12_2022_Chapter5/DirectX12_2022_Chapter5.cpp
@@ -248,13 +248,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	};
 
 	Vertex  vertices[] = {
-		{{0.0f, 0.0f, 0.0f},	{0.0f, 0.0f}}, // 左下
-		{{0.25f * (float)sqrt(3) / ratio,  -0.25f * (float)sqrt(3) + 0.5f , 0.0f},	{1.0f, 1.0f}}, // 左上 
+
+		//一つ目の図形
+		{{0.0f, 0.0f, 0.0f},	{0.0f, 0.0f}}, // 中心
+		{{0.25f * (float)sqrt(3) / ratio,  -0.25f * (float)sqrt(3) + 0.5f , 0.0f},	{1.0f, 1.0f}}, // 右上
 		{{ 0.25f / ratio, -0.25f * (float)sqrt(3), 0.0f},	{1.0f, 0.0f}}, // 右下
 		{{ -0.25f / ratio,  -0.25f * (float)sqrt(3), 0.0f},	{0.0f, 1.0f}}, // 右上
-		{{ -0.25f * (float)sqrt(3) / ratio,  -0.25f * (float)sqrt(3) + 0.5f , 0.0f},	{1.0f, 1.0f}},
-		{{ 0.0f, 0.5f , 0.0f},	{0.0f, 1.0f}},
-
+		{{ -0.25f * (float)sqrt(3) / ratio,  -0.25f * (float)sqrt(3) + 0.5f , 0.0f},	{1.0f, 1.0f}},  //左上
+		{{ 0.0f, 0.5f , 0.0f},	{0.0f, 1.0f}},  //上
+		
+		//二つ目の図形
 		{{ -0.9f, -0.5f, 0.0f} , {0.0f, 0.0f}},
 		{{ -0.9f,  0.4f, 0.0f} , {1.0f, 1.0f}},
 		{{ -0.5f,  -0.5f, 0.0f},	{1.0f, 0.0f}},
